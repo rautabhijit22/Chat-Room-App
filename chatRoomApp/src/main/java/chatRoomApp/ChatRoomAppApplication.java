467a942a85280d18e6317a11488b5fb6a60bb54d
rautabhijit22 <100755846+rautabhijit22@users.noreply.github.com>
2023-02-27 14:45:07 +0530

    Add files via upload

---
+++ chatRoomApp/src/main/java/chatRoomApp/ChatRoomAppApplication.java
@@ -0,0 +1,13 @@
+package chatRoomApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatRoomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatRoomAppApplication.class, args);
+	}
+
+}
---
+++ chatRoomApp/src/test/java/chatRoomApp/ChatRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package chatRoomApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
